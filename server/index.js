6722a17afde1d494f5485131d4b095b1c1ce4304
Annika83 <annika1983neumann@gmail.com>
2022-07-18 00:58:01 +0100

    debugged typos in app.js and index.js, installed mysql2 due to server connection issues, data still not being send to db, strack trace issues and error 1251

--- server/index.js
+++ server/index.js
@@ -1,38 +1,46 @@
+
+
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
-const PORT = 3001;
+const mysql = require("mysql2");
 const cors = require("cors");
+const PORT = 3001;
 
-app.use(cors())
-app.use(express.json())
+//found this on stackoverflow, might help
+const http = require("http");
+
+app.use(cors());
+app.use(express.json());
 
 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'password',
-    database: 'PasswordManager',
+    user: "root",
+    host: "localhost",
+    password: "password",
+    database: "PasswordManager",
 });
 
 // set up the post method to create a password
 
-app.post('/addPassword', (req, res) => {
-    const {password, website} = req.body
+app.post("/addpassword", (req, res) => {
+    const { password, website } = req.body;
 //first db query to insert password and website examples with the values
-    db.query("INSERT INTO password (password, website) VALUES (?,?)", 
+//Password = db table name
+    db.query(
+        "INSERT INTO Passwords (password, website) VALUES (?,?)", 
     [password, website], (err, result) => {
         //console log any errors if they occur
-        if (err){
-            console.log(err)
+        if (err) {
+            console.log(err);
         } else{
-            res.send("Success")
+            res.send("Success");
         }
-    })
+    }
+);
 
-})
+});
 
 
 app.listen(PORT, () =>{
-    console.log('Server is running')
+    console.log("Server is running");
 });
