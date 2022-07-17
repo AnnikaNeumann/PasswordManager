8ffe9eeba1a72f9d87ec9965475946126b4d11c4
Annika83 <annika1983neumann@gmail.com>
2022-07-17 23:43:46 +0100

    setup input form for passwords, trial and error sending data to db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const mysql = require('mysql')
-const PORT = 3001
+const mysql = require("mysql");
+const PORT = 3001;
+const cors = require("cors");
+
+app.use(cors())
+app.use(express.json())
+
 
 const db = mysql.createConnection({
     user: 'root',
@@ -10,9 +15,24 @@ const db = mysql.createConnection({
     database: 'PasswordManager',
 });
 
-app.get('/', (req, res) => {
-    res.send("Hello Annika");
+// set up the post method to create a password
+
+app.post('/addPassword', (req, res) => {
+    const {password, website} = req.body
+//first db query to insert password and website examples with the values
+    db.query("INSERT INTO password (password, website) VALUES (?,?)", 
+    [password, website], (err, result) => {
+        //console log any errors if they occur
+        if (err){
+            console.log(err)
+        } else{
+            res.send("Success")
+        }
+    })
+
 })
+
+
 app.listen(PORT, () =>{
     console.log('Server is running')
 });
